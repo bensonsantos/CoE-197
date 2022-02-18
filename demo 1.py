f79968bef2e6784d8310b8efa3377b145ecf1c97
bensonsantos <jrsantos24@up.edu.ph>
2022-02-18 23:30:27 +0800

    added

--- demo 1.py
+++ demo 1.py
@@ -52,3 +52,14 @@ def square_list(x):
 j = square_list(j)
 print(j)
 
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+    
+    def __str__(self):
+        return f"{self.name} is {self.age} years old"
+
+p = Person("benson", 6)
+print(p)
+
